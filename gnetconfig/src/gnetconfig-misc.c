fcc76e403ed3c1d6f5fd37271fdc5336cb26c32d
Priyank <priyank@frugalware.org>
2007-05-05 16:02:35 +0000

    gnetconfig: fix a logical bug in gnetconfig_get_wireless_mode()

--- gnetconfig/src/gnetconfig-misc.c
+++ gnetconfig/src/gnetconfig-misc.c
@@ -112,19 +112,19 @@ gnetconfig_profile_free (fwnet_profile_t *profile)
 int
 gnetconfig_get_wireless_mode (const char *mode)
 {
-	if (!strlen(mode) && (!strcmp(mode, "ad-hoc")))
+	if (strlen(mode) && (!strcmp(mode, "ad-hoc")))
 		return GN_AD_HOC;
-	else if (!strlen(mode) && (!strcmp(mode, "managed")))
+	else if (strlen(mode) && (!strcmp(mode, "managed")))
 		return GN_MANAGED;
-	else if (!strlen(mode) && (!strcmp(mode, "master")))
+	else if (strlen(mode) && (!strcmp(mode, "master")))
 		return GN_MASTER;
-	else if (!strlen(mode) && (!strcmp(mode, "monitor")))
+	else if (strlen(mode) && (!strcmp(mode, "monitor")))
 		return GN_MONITOR;
-	else if (!strlen(mode) && (!strcmp(mode, "repeater")))
+	else if (strlen(mode) && (!strcmp(mode, "repeater")))
 		return GN_REPEATER;
-	else if (!strlen(mode) && (!strcmp(mode, "secondary")))
+	else if (strlen(mode) && (!strcmp(mode, "secondary")))
 		return GN_SECONDARY;
-	else if (!strlen(mode) && (!strcmp(mode, "auto")))
+	else if (strlen(mode) && (!strcmp(mode, "auto")))
 		return GN_AUTO;
 	else
 		return -1;
