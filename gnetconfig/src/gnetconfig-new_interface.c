cb92183552532c1752631450cd561b4386306ac0
Priyank <priyank@frugalware.org>
2007-06-05 19:38:13 +0000

    gnetconfig: added 'new interface' dialog (2)

---
+++ gnetconfig/src/gnetconfig-new_interface.c
@@ -0,0 +1,152 @@
+/***************************************************************************
+ *  gnetconfig-new_interface.c
+ *  Author(s): Priyank Gosalia <priyankmg@gmail.com>
+ *  Copyright (C) 2007 Frugalware Developer Team
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#define _GNU_SOURCE
+#include <locale.h>
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+#include <gdk/gdkkeysyms.h>
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+#include "gnetconfig-new_interface.h"
+#include "gnetconfig-messages.h"
+#include "gnetconfig-misc.h"
+
+extern GladeXML 	*xml;
+extern fwnet_profile_t	*active_profile;
+
+GtkWidget *dialog;
+
+GtkWidget *gn_nconntype_combo;
+GtkWidget *gn_staticip_table;
+GtkWidget *gn_dhcp_table;
+GtkWidget *gn_dsl_table;
+GtkWidget *gn_nipaddress_entry;
+GtkWidget *gn_ngateway_entry;
+GtkWidget *gn_nnetmask_entry;
+GtkWidget *gn_ndhcp_hostname_entry;
+
+static void cb_gn_nconntype_changed (GtkComboBox *combo, gpointer data);
+static void cb_gn_new_int_save_clicked (GtkWidget *widget, gpointer data);
+static void cb_gn_new_int_autodetect_clicked (GtkWidget *widget, gpointer data);
+
+void
+gnetconfig_new_interface_dialog_setup (void)
+{
+	GtkWidget *widget;
+
+	dialog = glade_xml_get_widget (xml, "new_interface_dialog");
+
+	gn_staticip_table	= glade_xml_get_widget (xml, "fwn_staticip_table2");
+	gn_dhcp_table		= glade_xml_get_widget (xml, "fwn_dhcp_table2");
+	gn_nconntype_combo 	= glade_xml_get_widget (xml, "fwn_conntype_list2");
+	gn_nipaddress_entry 	= glade_xml_get_widget (xml, "fwn_ip2");
+	gn_nnetmask_entry 	= glade_xml_get_widget (xml, "fwn_netmask2");
+	gn_ngateway_entry 	= glade_xml_get_widget (xml, "fwn_gateway2");
+
+	/* setup signals and callbacks */
+	widget = glade_xml_get_widget (xml, "fwn_new_int_save");
+	g_signal_connect (G_OBJECT(widget),
+			"clicked",
+			G_CALLBACK(cb_gn_new_int_save_clicked),
+			NULL);
+
+	widget = glade_xml_get_widget (xml, "fwn_if_autodetect");
+	g_signal_connect (G_OBJECT(widget),
+			"clicked",
+			G_CALLBACK(cb_gn_new_int_autodetect_clicked),
+			NULL);
+
+	g_signal_connect (G_OBJECT(gn_nconntype_combo),
+			"changed",
+			G_CALLBACK(cb_gn_nconntype_changed),
+			NULL);
+
+	return;
+}
+		
+void
+gnetconfig_new_interface_dialog_show (void)
+{
+	if (!GTK_WIDGET_VISIBLE(dialog))
+	{
+		gtk_widget_show (dialog);
+		gtk_window_present (GTK_WINDOW(dialog));
+	}
+	else
+	{
+		gtk_window_present (GTK_WINDOW(dialog));
+	}
+	return;
+}
+
+/* CALLBACKS */
+
+static void
+cb_gn_nconntype_changed (GtkComboBox *combo, gpointer data)
+{
+	switch (gtk_combo_box_get_active (combo))
+	{
+		gtk_entry_set_text (GTK_ENTRY(gn_nipaddress_entry), "");
+		gtk_entry_set_text (GTK_ENTRY(gn_nnetmask_entry), "");
+		gtk_entry_set_text (GTK_ENTRY(gn_ngateway_entry), "");
+		
+		case GN_DHCP: /* DHCP */
+			gtk_widget_show (gn_dhcp_table);
+			gtk_widget_hide (gn_staticip_table);
+			//gtk_widget_hide (gn_dsl_table);
+			break;
+
+		case GN_STATIC: /* Static ip */
+			gtk_widget_show (gn_staticip_table);
+			gtk_widget_hide (gn_dhcp_table);
+			gtk_widget_set_sensitive (gn_nipaddress_entry, TRUE);
+			gtk_widget_set_sensitive (gn_nnetmask_entry, TRUE);
+			gtk_widget_set_sensitive (gn_ngateway_entry, TRUE);
+			break;
+
+		case GN_DSL: /* DSL */
+			//gtk_widget_show (gn_dsl_table);
+			break;
+
+		case GN_LO: /* lo */
+			break;
+	}
+
+	return;
+}
+
+static void
+cb_gn_new_int_save_clicked (GtkWidget *widget, gpointer data)
+{
+	return;
+}
+
+static void
+cb_gn_new_int_autodetect_clicked (GtkWidget *widget, gpointer data)
+{
+	return;
+}
+
---
+++ gnetconfig/src/gnetconfig-new_interface.h
@@ -0,0 +1,15 @@
+#ifndef __GNETCONFIG_NEW_INTERFACE_H__
+#define __GNETCONFIG_NEW_INTERFACE_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
+#include <gtk/gtk.h>
+#include <libfwnetconfig.h>
+
+void gnetconfig_new_interface_dialog_setup (void);
+void gnetconfig_new_interface_dialog_show (void);
+
+#endif
