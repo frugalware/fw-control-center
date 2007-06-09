9772c218899e24b3d793402a7139e42e75842c30
Priyank <priyank@frugalware.org>
2007-06-09 19:40:31 +0000

    gnetconfig: separate source file for new profile handling.

---
+++ gnetconfig/src/gnetconfig-new_profile.h
@@ -0,0 +1,12 @@
+#ifndef __GNETCONFIG_NEW_PROFILE_H__
+#define __GNETCONFIG_NEW_PROFILE_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <locale.h>
+#include <gtk/gtk.h>
+#include <libfwnetconfig.h>
+
+#endif
