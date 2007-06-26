ec1f9071f7231c39b3e863eabad466d0fed6d7a3
Priyank <priyank@frugalware.org>
2007-06-26 15:09:46 +0000

    gnetconfig: autogen.sh: the --dist check should be on top of everything else. thx vmiklos.

--- gnetconfig/autogen.sh
+++ gnetconfig/autogen.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -e
 
-cat /usr/share/aclocal/libtool.m4 >> aclocal.m4
-libtoolize -f -c
-aclocal --force
-autoheader -f
-autoconf -f
-cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
-cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
-automake -a -c --gnu --foreign
-
 if [ "$1" == "--dist" ]; then
         ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
         darcs changes >../_darcs/pristine/ChangeLog
@@ -19,6 +10,15 @@ if [ "$1" == "--dist" ]; then
         #mv ../gnetconfig-$ver.tar.gz.asc ../..
         exit 0
 fi
+
+cat /usr/share/aclocal/libtool.m4 >> aclocal.m4
+libtoolize -f -c
+aclocal --force
+autoheader -f
+autoconf -f
+cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
+cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
+automake -a -c --gnu --foreign
 
 if [ "$1" == "--darcs" ]; then
 		rm -rf autom4te.cache
