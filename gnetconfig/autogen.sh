dfe47a1b173132e8c1760cd3d7fe348d47b62bcb
Priyank <priyank@frugalware.org>
2007-06-26 14:03:51 +0000

    gnetconfig: misc fixes * autogen.sh: minor bugfix in --dist * gnetconfig.c: fix copyright

--- gnetconfig/autogen.sh
+++ gnetconfig/autogen.sh
@@ -11,11 +11,11 @@ automake -a -c --gnu --foreign
 
 if [ "$1" == "--dist" ]; then
         ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
-        darcs changes >_darcs/pristine/ChangeLog
+        darcs changes >../_darcs/pristine/ChangeLog
         darcs dist -d gnetconfig-$ver
-        rm _darcs/pristine/ChangeLog
+        rm ../_darcs/pristine/ChangeLog
         gpg --comment "See http://ftp.frugalware.org/pub/README.GPG for info" \
-                -ba -u 20F55619 gnetconfig-$ver.tar.gz
-        mv gnetconfig-$ver.tar.gz.asc ..
+                -ba -u 20F55619 ../gnetconfig-$ver.tar.gz
+        #mv ../gnetconfig-$ver.tar.gz.asc ../..
         exit 0
 fi
