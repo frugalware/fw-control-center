96fce76bbe6f6ae73526e3a12f0339bf69a23041
VMiklos <vmiklos@frugalware.org>
2007-06-28 17:42:01 +0000

    autogen.sh: cosmetics converted 4/8/16 spaces of identation to tabs

--- gnetconfig/autogen.sh
+++ gnetconfig/autogen.sh
@@ -1,24 +1,24 @@
 #!/bin/sh -e
 
 generate_pot() {
-    cd po/
-    mv Makevars Makevars.tmp
-    cp /usr/bin/intltool-extract ./
-    intltool-update --pot --gettext-package=gnetconfig
-    rm intltool-extract
-    mv Makevars.tmp Makevars
-    cd - > /dev/null
+	cd po/
+	mv Makevars Makevars.tmp
+	cp /usr/bin/intltool-extract ./
+	intltool-update --pot --gettext-package=gnetconfig
+	rm intltool-extract
+	mv Makevars.tmp Makevars
+	cd - > /dev/null
 }
 
 if [ "$1" == "--dist" ]; then
-        ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
-        darcs changes >../_darcs/pristine/ChangeLog
-        darcs dist -d gnetconfig-$ver
-        rm ../_darcs/pristine/ChangeLog
-        gpg --comment "See http://ftp.frugalware.org/pub/README.GPG for info" \
-                -ba -u 20F55619 ../gnetconfig-$ver.tar.gz
-        #mv ../gnetconfig-$ver.tar.gz.asc ../..
-        exit 0
+	ver=`grep AC_INIT configure.ac|sed 's/.*, \([0-9\.]*\), .*/\1/'`
+	darcs changes >../_darcs/pristine/ChangeLog
+	darcs dist -d gnetconfig-$ver
+	rm ../_darcs/pristine/ChangeLog
+	gpg --comment "See http://ftp.frugalware.org/pub/README.GPG for info" \
+		-ba -u 20F55619 ../gnetconfig-$ver.tar.gz
+	#mv ../gnetconfig-$ver.tar.gz.asc ../..
+	exit 0
 fi
 
 cat /usr/share/aclocal/libtool.m4 >> aclocal.m4
@@ -34,6 +34,6 @@ cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
 automake -a -c --gnu --foreign
 
 if [ "$1" == "--darcs" ]; then
-		rm -rf autom4te.cache
+	rm -rf autom4te.cache
 fi
 
