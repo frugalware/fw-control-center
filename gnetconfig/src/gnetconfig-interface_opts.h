3ebb58b293c60281a116d517b205ee49f400f987
Priyank <priyank@frugalware.org>
2007-06-28 15:26:28 +0000

    gnetconfig: fix another implicit declaration warning.

--- gnetconfig/src/gnetconfig-interface_opts.h
+++ gnetconfig/src/gnetconfig-interface_opts.h
@@ -8,8 +8,11 @@
 #include <locale.h>
 #include <libintl.h>
 #include <gtk/gtk.h>
+#include <libfwnetconfig.h>
 
 #define _(string) gettext (string)
+
+void gnetconfig_populate_opts (fwnet_interface_t *);
 
 void gnetconfig_interface_opts_init (void);
 
