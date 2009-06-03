8bf4be6f3ab0cca4d36d13a09acacfe589144238
Priyank <priyank@frugalware.org>
2009-06-03 12:55:59 +0530

    xfce4-gfpm-plugin: new build system

--- xfce4-gfpm-plugin/autogen.sh
+++ xfce4-gfpm-plugin/autogen.sh
@@ -5,7 +5,7 @@ gen_pot()
 	cd po/
 	mv Makevars Makevars.tmp
 	cp /usr/bin/intltool-extract ./
-	intltool-update --pot --gettext-package=gfpm-mcs-plugin
+	intltool-update --pot --gettext-package=xfce4-gfpm-plugin
 	rm intltool-extract
 	mv Makevars.tmp Makevars
 	cd - >/dev/null
@@ -18,8 +18,8 @@ import_pootle()
 		: > po/LINGUAS
 		for i in $(/bin/ls $po_dir/fwcontrolcenter)
 		do
-			[ -e $po_dir/fwcontrolcenter/$i/gfpm-mcs-plugin.po ] || continue
-			cp $po_dir/fwcontrolcenter/$i/gfpm-mcs-plugin.po po/$i.po
+			[ -e $po_dir/fwcontrolcenter/$i/xfce4-gfpm-plugin.po ] || continue
+			cp $po_dir/fwcontrolcenter/$i/xfce4-gfpm-plugin.po po/$i.po
 			if msgfmt -c --statistics -o po/$i.gmo po/$i.po; then
 				echo $i >> po/LINGUAS
 			else
@@ -43,16 +43,15 @@ if [ "$1" == "--dist" ]; then
 	ver=$ver.`grep version_minor -m1 configure.in | sed 's/.*, \[\(.*\)].*/\1/'`
 	ver=$ver.`grep version_micro -m1 configure.in | sed 's/.*, \[\(.*\)].*/\1/'`
 	git archive --format=tar --prefix=gfpm-mcs-plugin-$ver/ HEAD | tar xf -
-	git log --no-merges |git name-rev --tags --stdin > gfpm-mcs-plugin-$ver/ChangeLog
-	cd gfpm-mcs-plugin-$ver
+	git log --no-merges |git name-rev --tags --stdin > xfce4-gfpm-plugin-$ver/ChangeLog
+	cd xfce4-gfpm-plugin-$ver
 	./autogen.sh --git
 	rm -rf autom4te.cache
 	cd ..
-	tar czf gfpm-mcs-plugin-$ver.tar.gz gfpm-mcs-plugin-$ver
-	rm -rf gfpm-mcs-plugin-$ver
+	tar czf xfce4-gfpm-plugin-$ver.tar.gz xfce4-gfpm-plugin-$ver
+	rm -rf xfce4-gfpm-plugin-$ver
 	gpg --comment "See http://ftp.frugalware.org/pub/README.GPG for info" \
-		-ba -u 20F55619 gfpm-mcs-plugin-$ver.tar.gz
-	#mv gnetconfig-$ver.tar.gz.asc ../..
+		-ba -u 20F55619 xfce4-gfpm-plugin-$ver.tar.gz
 	exit 0
 fi
 
@@ -67,7 +66,6 @@ autoheader -f
 autoconf -f
 cp -f $(dirname $(which automake))/../share/automake/mkinstalldirs ./
 cp -f $(dirname $(which automake))/../share/gettext/config.rpath ./
-NOCONFIGURE=1 xdt-autogen
 automake -a -c --gnu --foreign
 if [ "$1" == "--git" ]; then
 	rm -rf autom4te.cache
